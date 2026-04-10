9d6a348e0036f27e490b7be5713104bdb3337ea8
yabingrigory8893 <yabingrigory3@gmail.com>
2026-04-10 03:01:29 -0700

    Update AreaCalculator.java\

--- src/AreaCalculator.java
+++ src/AreaCalculator.java
@@ -1,21 +1,38 @@
 import java.util.Scanner;
 
 public class AreaCalculator {
-    
-    // TODO: Define calculateArea(float radius) -> returns area of circle
-    // Use Math.PI or 3.14159f for pi
+    public float calculateArea(float radius) {
+        return (float) (Math.PI * radius * radius);
+    }
 
-    // TODO: Define calculateArea(float length, float breadth) -> returns area of rectangle
+    public float calculateArea(float length, float breadth) {
+        return length * breadth;
+    }
 
-    // TODO: Define calculateArea(double base, double height) -> returns area of triangle
+    public double calculateArea(double base, double height) {
+        return 0.5 * base * height;
+    }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        // TODO: Read input for Circle (radius)
-        // TODO: Read input for Rectangle (length, breadth)
-        // TODO: Read input for Triangle (base, height)
-        
-        // TODO: Call methods and print results formatted to 2 decimal places
-        // Hint: Use System.out.printf("%.2f\n", area);
+        try (Scanner sc = new Scanner(System.in)) {
+            AreaCalculator calc = new AreaCalculator();
+
+            if (sc.hasNextFloat()) {
+                float radius = sc.nextFloat();
+                System.out.printf("%.2f\n", calc.calculateArea(radius));
+            }
+
+            if (sc.hasNextFloat()) {
+                float length = sc.nextFloat();
+                float breadth = sc.nextFloat();
+                System.out.printf("%.2f\n", calc.calculateArea(length, breadth));
+            }
+
+            if (sc.hasNextDouble()) {
+                double base = sc.nextDouble();
+                double height = sc.nextDouble();
+                System.out.printf("%.2f\n", calc.calculateArea(base, height));
+            }
+        }
     }
 }
